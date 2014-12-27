afcd7ba4b814eee4287adf506a036eab9698939f
Yago Duran Cid <kindermatik@linux.site>
2014-12-27 11:27:35 +0100

    prelim work for second submission

---
+++ Outliers_Estimation.R
@@ -0,0 +1,33 @@
+drivers.sample<-function(wd1="~/kaggle/AXA/drivers/",wd2="~/kaggle/AXA"){
+#Creates a sample of drives
+#First row contains all drivers
+#Following 4 rows contain random samples of other drivers
+#there might be some columns that contain repeated the first driver
+  
+  setwd(wd1)
+  lfiles<-list.files()
+  setwd(wd2)
+  
+  for (i in 1:length(lfiles)){
+    set.seed(i)
+    rnd.sample<-sample(lfiles,4)
+    drivers.column<-c(lfiles[i],rnd.sample)
+    
+    if (i==1){
+      sample.drivers<-data.frame(drivers.column)
+    } else {
+      sample.drivers<-cbind(sample.drivers,drivers.column)
+    }
+  }
+  
+  colnames(sample.drivers)<-lfiles
+  sample.drivers
+}
+
+outlier.rf<-function(sample,drivers=c(1:10)){
+  library(randomForest)
+  
+  for (i in 1:length(drivers)){
+    
+  }
+}
